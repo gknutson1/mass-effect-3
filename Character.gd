7b4483181f6d4fa43a7379dfa427ce8b7878d17e
Rigormortis115 <williamacarson1@outlook.com>
2024-04-13 16:57:26 -0700

    Battling works again

--- Character.gd
+++ Character.gd
@@ -11,16 +11,22 @@ var defense: int
 var char_class: class_options
 
 @onready 
-var health_label = get_node("../HealthLabel")
+var health_label = get_node("HealthLabel")
 @onready 
-var attack_label = get_node("../AttackLabel")
+var attack_label = get_node("AttackLabel")
 
-func _init(char_name: String, char_class: class_options, health: int, attack: int, defense: int):
+func initialize(char_name: String, char_class: class_options, health: int, attack: int, defense: int):
 	self.char_name = char_name
 	self.char_class = char_class
 	self.health = health
 	self.attack = attack
 	self.defense = defense
+	update_labels()
+
+func update_labels():
+	if is_inside_tree():
+		health_label.text = str(health)
+		attack_label.text = str(attack)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -32,7 +38,3 @@ func _ready():
 func _process(delta):
 	update_labels()
 	pass
-
-func update_labels():
-	health_label.text = str(health)
-	attack_label.text = str(attack)
