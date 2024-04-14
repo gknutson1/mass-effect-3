613245bab2851181336bfb828ea4cc1fb5b17530
Rigormortis115 <williamacarson1@outlook.com>
2024-04-13 17:44:35 -0700

    Got some nice looking animations working

--- BattleManager.gd
+++ BattleManager.gd
@@ -7,10 +7,14 @@ class_name BattleManager
 # Check if the team can be "crunched"
 #
 
+enum end_state {NONE, T1WIN, T2WIN, DRAW}
+
 var team1 = []
 var team2 = []
 
 var character_asset = load("res://char.tscn")
+
+var move_orders = []
 
 func _ready():
 	# Create characters
@@ -19,6 +23,9 @@ func _ready():
 	team1.append(create_character("Archer1", Character.class_options.ARCHER, 100, 15, 5))
 	
 	team2.append(create_character("Archer2", Character.class_options.ARCHER, 100, 15, 5))
+	team2.append(create_character("Archer3", Character.class_options.ARCHER, 100, 15, 5))
+	team2.append(create_character("Archer4", Character.class_options.ARCHER, 100, 15, 5))
+	team2.append(create_character("Archer5", Character.class_options.ARCHER, 100, 15, 5))
 	
 	# Start battle
 	start_battle()
@@ -32,10 +39,10 @@ func create_character(name: String, class_option: Character.class_options, hp: i
 func start_battle():
 	# Position characters on the battlefield
 	for i in range(team1.size()):
-		team1[i].position = Vector2((i + 1) * 200, 100)
+		team1[i].position = Vector2((i + 1) * -200, 100)
 		
 	for i in range(team2.size()):
-		team2[i].position = Vector2((i + 1) * -200, 100)
+		team2[i].position = Vector2((i + 1) * 200, 100)
 		
 	# Simulate rounds
 	while can_battle():
@@ -43,9 +50,21 @@ func start_battle():
 		await get_tree().create_timer(0.4).timeout
 	
 func can_battle() -> bool:
-	print("Can battle!")
+	var busy_characters = are_characters_busy()
 	return team1.size() > 0 && team2.size() > 0
-	
+
+func are_characters_busy() -> bool:
+	for character in team1:
+		if character.busy:
+			return true
+			
+	for character in team1:
+		
+		if character.busy:
+			return true
+			
+	return false
+
 func fight_round():
 	var attacker1 = team1[0] as Character
 	var attacker2 = team2[0] as Character
@@ -54,20 +73,58 @@ func fight_round():
 	attacker1.health -= attacker2.attack
 	attacker2.health -= attacker1.attack
 	
+	#NOTE: We will have to repeat. Character abilities may trigger character abilities. Can't loop once.
 	#for character in team1:
 		#character.execute_abilities(self)
 		#
 	#for character in team2:
 		#character.execute_abilities(self)
 	
-	var kill1 = team1.filter(is_alive)
-	var kill2 = team2.filter(is_alive)
+	var kill1 = team1.filter(is_dead)
+	var kill2 = team2.filter(is_dead)
 	
 	var kill = kill1 + kill2
 	
+	for char in kill:
+		team1.erase(char)
+		team2.erase(char)
+		
+	assign_move_orders()
+		
+	var winner = declare_winner()
 	
+	if winner == end_state.T1WIN:
+		print("Team 1 wins!")
+	
+	if winner == end_state.T2WIN:
+		print("Team 2 wins!")
+	
+	if winner == end_state.DRAW:
+		print("Draw!")
 	
 	pass
 
-func is_alive(character: Character) -> bool:
-	return character.health > 0
+func is_dead(character: Character) -> bool:
+	return character.health <= 0
+
+func declare_winner() -> end_state:
+	if team1.size() == 0 and team2.size() == 0:
+		return end_state.DRAW
+		
+	if team1.size() == 0:
+		return end_state.T2WIN
+		
+	if team2.size() == 0:
+		return end_state.T1WIN
+		
+	return end_state.NONE
+
+func assign_move_orders():
+	for i in range(team1.size()):
+		var character = team1[i] as Character
+		character.target_position = Vector2((i + 1) * -200, 100)
+		
+	for i in range(team2.size()):
+		var character = team2[i] as Character
+		character.target_position = Vector2((i + 1) * 200, 100)
+		
